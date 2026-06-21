4d6ecb9d30eb3082b645e05284c760ec4d2d46c2
antebustruc-cloud <ante.bustruc@gmail.com>
2026-06-21 13:20:08 +0200

    Fix Leaflet map marker icons, add payment+ratings roadmap to spec

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import L from 'leaflet';
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png',
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
